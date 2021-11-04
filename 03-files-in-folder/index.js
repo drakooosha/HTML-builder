1725da2a491aa72bd0225ce0f34ed4f2f057a2fd
Kirill Kopteloy <kopteloy@gmail.com>
2021-11-05 00:20:02 +0300

    feat: added 3 tast

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readdir('03-files-in-folder/secret-folder' , {withFileTypes : true}, (err,files)=>{
+  for(let i = 0; i < files.length; i++) {
+    fs.stat('03-files-in-folder/secret-folder/' + files[i].name, (err,stats)=>{
+      if(!stats.isDirectory()) {
+        console.log(`${files[i].name.split('.')[0]} - ${path.extname(files[i].name).split('.')[1]} - ${(stats.size)/1000}kb`)
+      }
+    });
+  }
+})
