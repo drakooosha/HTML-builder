d54ec20cae44b13039aa3f4b4159d8f4ce8016cf
Kirill Kopteloy <kopteloy@gmail.com>
2021-11-05 01:22:22 +0300

    feat: done 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
 
+fs.mkdir('04-copy-directory/files-copy',{recursive : true}, (error)=>{
+  if(error) {
+    throw error;
+  }
+});
+
+
+fs.readdir('04-copy-directory/files' , {withFileTypes : true}, (err,files)=>{
+  for(let i = 0; i < files.length; i++) {
+    fs.copyFile('04-copy-directory/files/' + files[i].name, '04-copy-directory/files-copy/' + files[i].name,(err)=>{
+      if(err) {
+        console.log(err)
+      }
+    });
+  }
+})
