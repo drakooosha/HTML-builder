af86878b338c721a9eb6fde595da74387ffcabbb
drakooosha <77409602+drakooosha@users.noreply.github.com>
2021-11-09 20:08:49 +0300

    fix kb

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ fs.readdir('03-files-in-folder/secret-folder' , {withFileTypes : true}, (err,fil
   for(let i = 0; i < files.length; i++) {
     fs.stat('03-files-in-folder/secret-folder/' + files[i].name, (err,stats)=>{
       if(!stats.isDirectory()) {
-        console.log(`${files[i].name.split('.')[0]} - ${path.extname(files[i].name).split('.')[1]} - ${(stats.size)/1000}kb`)
+        console.log(`${files[i].name.split('.')[0]} - ${path.extname(files[i].name).split('.')[1]} - ${(stats.size)/1024}kb`)
       }
     });
   }
